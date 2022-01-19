78ad081d60ff3705413ae8ede5bb6961e367f85f
Jennifer LeBaron <JenLeBaron@msn.com>
2022-01-19 11:07:35 -0800

    finsihed week1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+const PORT = process.env.PORT || 5001; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
 
@@ -33,7 +33,7 @@ app
   // For view engine as hbs (Handlebars)
   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
   //.set('view engine', 'hbs')
-  .use(bodyParser({ extended: false })) // For parsing the body of a POST
+  .use(bodyParser.urlencoded({ extended: false })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
